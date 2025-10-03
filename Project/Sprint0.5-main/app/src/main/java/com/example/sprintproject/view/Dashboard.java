968ff3bc02a0b0493785efa16ca42ef4e6bc5fef
Kushi Kashyap <kushikash06@gmail.com>
2025-10-03 15:09:45 -0400

    updated UI and button functionality for addBudget and addExpense

--- Project/Sprint0.5-main/app/src/main/java/com/example/sprintproject/view/Dashboard.java
+++ Project/Sprint0.5-main/app/src/main/java/com/example/sprintproject/view/Dashboard.java
@@ -15,6 +15,9 @@ public class Dashboard extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_dashboard);
+
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_dashboard);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
--- Project/Sprint0.5-main/app/src/main/java/com/example/sprintproject/view/ExpenseLog.java
+++ Project/Sprint0.5-main/app/src/main/java/com/example/sprintproject/view/ExpenseLog.java
@@ -1,5 +1,6 @@
 package com.example.sprintproject.view;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
@@ -16,12 +17,20 @@ public class ExpenseLog extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_expense_log);
+        addExpense = findViewById(R.id.addExpense);
+
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_expense_log);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        addExpense.setOnClickListener(v -> {
+            startActivity(new Intent(ExpenseLog.this, ExpenseCreation.class));
         });
     }
 }
