3b4d4629987b5d3c5cef235254d51b773284e524
sririthishpalani-max <sririthishpalani@gmail.com>
2025-10-03 16:35:59 -0400

    made start page opening page

--- Project/Sprint0.5-main/app/src/main/java/com/example/sprintproject/view/MainActivity.java
+++ Project/Sprint0.5-main/app/src/main/java/com/example/sprintproject/view/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.sprintproject.view;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.sprintproject.R;
 
@@ -12,5 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnStart = findViewById(R.id.btnStart);
+        Button btnQuit = findViewById(R.id.btnQuit);
+
+
+        btnStart.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+        });
+
+
+        btnQuit.setOnClickListener(v -> {
+            finishAffinity();
+            System.exit(0);
+        });
     }
 }
+
