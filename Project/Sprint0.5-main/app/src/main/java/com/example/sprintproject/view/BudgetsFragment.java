8db075ae2a915b1f8582afa281e618a74d77eee4
Kushi Kashyap <kushikash06@gmail.com>
2025-10-17 21:33:31 -0400

    save changes

--- Project/Sprint0.5-main/app/src/main/java/com/example/sprintproject/view/BudgetsFragment.java
+++ Project/Sprint0.5-main/app/src/main/java/com/example/sprintproject/view/BudgetsFragment.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.core.graphics.Insets;
@@ -14,6 +15,13 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
 
 import com.example.sprintproject.R;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.Locale;
 
 public class BudgetsFragment extends Fragment {
 
@@ -44,8 +52,37 @@ public class BudgetsFragment extends Fragment {
                     .setView(popupView)
                     .create();
 
-            Button closeButton = popupView.findViewById(R.id.closeButton);
-            closeButton.setOnClickListener(view1 -> dialog.dismiss());
+            EditText name = popupView.findViewById(R.id.nameInput);
+            EditText amountInput = popupView.findViewById(R.id.amountInput);
+            EditText frequency = popupView.findViewById(R.id.frequencyInput);
+            EditText startDateInput = popupView.findViewById(R.id.startDateInput);
+
+            Button createButton = popupView.findViewById(R.id.createButton);
+            createButton.setOnClickListener(
+                    view1 -> {
+                        String amountText = amountInput.getText().toString();
+                        int amount = Integer.parseInt(amountText);
+                        String startDateText = startDateInput.getText().toString();
+                        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+                        Date currentDate = new Date();
+                        Date startDate;
+                        try {
+                            startDate = format.parse(startDateText);
+                            if (amount <= 0) {
+                                amountInput.setError("Start date must be in the future");
+
+                            } else if (startDate.compareTo(currentDate) < 0) {
+                                startDateInput.setError("Start date must be in the future");
+                            }
+                        } catch (ParseException e) {
+                            startDateInput.setError("Start date must be in correct format.");
+                        }
+                        dialog.dismiss();
+                    }
+            );
+
+            Button cancelButton = popupView.findViewById(R.id.cancelButton);
+            cancelButton.setOnClickListener(view1 -> dialog.dismiss());
 
             dialog.show();
         });
